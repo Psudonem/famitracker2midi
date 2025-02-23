191e10c5e514b86e5bbe3f21fd6b1fb19cc20c33
Psudonem <jaseystudios@gmail.com>
2025-02-23 11:41:48 -0500

    we can now load the whole pattern and pattern order. this is the minimum needed to build a full completish midi

--- tdd/protomaker.py
+++ tdd/protomaker.py
@@ -1,9 +1,44 @@
+import json
+
+
+
 file = open("dragon world.txt", 'r')
 txtfile = file.read()
 file.close()
+output={}
+output['order']=[]
+output['patterns']={}
+curpattern =None
+for line in txtfile.split("\n"):
+	if("ORDER" in line[:7]): # loading orders
+		output['order'].append(line.split(": ")[-1].split(" "))
+	if("PATTERN" in line[:7]):
+		curpattern = line[8:10]
+		output['patterns'][curpattern]=[]
+	if("ROW" in line[:3]): #loading patterns
+		for channel in line.split(": ")[1:]:
+			entry={}
+			for i,param in enumerate(channel.split(" ")):
+
+				row = line[4:6]
+				if(i==0 and param!="..."): #note
+					entry['note']=param
+				if(i==1 and param!=".."): #instrument
+					entry["instrument"]=param
+				if(i==2 and param!="."): #volume
+					entry['volume']=param
+				if(i>2 and param!="..." and len(param)==3): # effect
+					if 'fx' not in entry:
+						entry['FX']=[]
+					entry['FX'].append(param)
+			if entry!={}:
+				output['patterns'][curpattern].append([row, entry])
+
 
 # loading patterns
-for line in txtfile.split("\n"):
-	print("PATTERN" in line[:7])
 
-	#WIP.. dont mess with this file yet
+
+finaltext = json.dumps(output)
+f = open("output.json","w")
+f.write(finaltext)
+f.close()
